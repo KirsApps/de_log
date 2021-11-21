1388124029baa4532cfc644a1edd4b7c112e47e4
Arthur Kirsanov <kirsappsinc@gmail.com>
2021-11-21 18:46:08 +0300

    100% test coverage

--- test/de_log_test.dart
+++ test/de_log_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:data_queue/data_queue.dart';
 import 'package:de_log/de_log.dart';
 import 'package:test/test.dart';
@@ -37,10 +39,19 @@ void main() {
   final bufferHandler = BufferHandler<String>();
   final log = DeLog<String>([bufferHandler, bufferQueueHandler]);
   log.trace('trace');
-  log.fatal('fatal');
-  log.error('error');
+  log.debug('debug');
+  log.info('info');
   log.warn('warning');
-  final expected = ['trace', 'fatal', 'error', 'warning'];
+  log.error('error');
+  log.fatal('fatal');
+  final expected = [
+    'trace',
+    'debug',
+    'info',
+    'warning',
+    'error',
+    'fatal',
+  ];
   test('log buffer', () {
     expect(bufferHandler.buffer, equals(expected));
   });
@@ -51,5 +62,18 @@ void main() {
     await log.dispose();
     expect(bufferHandler.isDisposed, equals(true));
     expect(bufferQueueHandler.isDisposed, equals(true));
+  });
+  test('Severity levels', () async {
+    expect(Level.trace.severityLevel, equals(300));
+    expect(Level.debug.severityLevel, equals(600));
+    expect(Level.info.severityLevel, equals(900));
+    expect(Level.warn.severityLevel, equals(1200));
+    expect(Level.error.severityLevel, equals(1500));
+    expect(Level.fatal.severityLevel, equals(1800));
+  });
+  test('The LogRecord', () async {
+    final record = LogRecord('test', name: 'name');
+    // Skip time
+    expect(record.toString().split(' - ')[1], equals('[name] test'));
   });
 }
